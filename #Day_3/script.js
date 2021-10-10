7d0d46ebffd3aafd7416235c22a98ea4731e96c1
sajda kabir <sajda.kbir@gmail.com>
2021-10-11 00:42:45 +0530

    added #Day_3 folder , css variables and js

---
+++ #Day_3/script.js
@@ -0,0 +1,13 @@
+const inputs=document.querySelectorAll('.controls input')
+
+function handleChanges() {
+     const suffix=this.dataset.sizing || '';
+       
+     document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix)
+}
+
+
+
+
+inputs.forEach(input =>input.addEventListener('change',handleChanges))
+inputs.forEach(input=> input.addEventListener('mousemove',handleChanges))
