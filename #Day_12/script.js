0b19bdf613f41012e64911450ffb3ca5e5558181
sajdakabir <sajda.kbir.com>
2022-11-11 01:30:09 +0530

    added the images

--- #Day_12/script.js
+++ #Day_12/script.js
@@ -1,12 +1,10 @@
-const pressed=[];
-const secretCode='sajda';
-window.addEventListener('keyup',(e) =>{
-    console.log(e.key)
-    pressed.push(e.key);
-    pressed.splice(-secretCode.length -1 ,pressed.length-secretCode.length);
-    if(pressed.join('').includes(secretCode)){
-        console.log('DING DING!');
-        cornify_add();
-    }
-    console.log(pressed);
-})
+const pressed = []
+	const secretcode = 'sajda'
+	window.addEventListener('keyup', (e)=>{
+		pressed.push(e.key)
+		pressed.splice(-secretcode.length - 1, pressed.length - secretcode.length)
+		console.log(pressed)
+		if (pressed.join('') === secretcode){
+		cornify_add()
+	}
+	})
